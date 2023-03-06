8b72aad2a804803b4bbc82a3ba576d631f848e83
Rahi <125520697+RahiMisra@users.noreply.github.com>
2023-03-06 15:53:57 -0800

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+class Main {
+
+  public static void main(String[] args) {
+    
+    Scanner KB = new Scanner(System.in);
+  
+    boolean menu = true;
+    
+    while(menu) {
+      char selection;
+      System.out.println();
+
+      System.out.println("WELCOME TO THE BUG-BYTE HOTEL RESERVATION SYSTEM");
+      System.out.println("-------------------------------------------------");
+      System.out.println("Please select from the menu below");
+      System.out.println("A.Book a room");
+
+      System.out.print("Enter Selection: ");
+
+      selection = KB.next().charAt(0);
+
+      switch(selection){
+        case 'A':
+          CheckIn();
+          break;
+          default:
+          System.out.println("Invaild Selection");
+      }
+    }
+    KB.close();
+    
+  }
+
+  public static void CheckIn(){
+    try {
+      BufferedWriter writer = new BufferedWriter(new FileWriter("HotelInput.txt"));
+         
+    Scanner KB = new Scanner(System.in);
+    
+    System.out.print("Please enter your full name separated by '_': ");
+    String name = KB.next();
+    
+    System.out.print("Please enter your mobile number: ");
+    int phoneNum = KB.nextInt();
+    
+    System.out.print("Please enter your email address: ");
+    String email = KB.next();
+
+    Customer customer = new Customer(name, phoneNum, email);
+
+    writer.write(customer.name + ", ");
+    writer.write(String.valueOf(customer.phoneNum) + ", ");
+    writer.write(customer.email);
+
+    System.out.println("You have entered " +name+ ", " +phoneNum+ ", and "+email);
+
+    KB.close();
+    writer.close();
+  } catch (IOException e) {
+    e.printStackTrace();
+  }
+  }
+}
