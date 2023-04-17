9bd9a3447fab00de58d66291a5df017a77656fb7
Asim H. Sheikh <98243280+CodeSheikh@users.noreply.github.com>
2023-04-17 16:59:11 -0700

    Update replitMain.java
    Added a edit user account panel once user logs in

--- replitMain.java
+++ replitMain.java
@@ -1,11 +1,12 @@
 
-/*import javax.swing.*;
+/*
+
+import javax.swing.*;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.event.*;
-
 
 public class HotelReservationSystem extends JFrame {
     private JButton loginButton, createAccountButton;
@@ -19,7 +20,6 @@ public class HotelReservationSystem extends JFrame {
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-    
         welcomeLabel = new JLabel("Welcome to Bug-Byte Hotel Reservation System");
         welcomeLabel.setFont(new Font("Arial", Font.BOLD, 20));
         usernameLabel = new JLabel("Username:");
@@ -57,7 +57,6 @@ public class HotelReservationSystem extends JFrame {
         panel.add(createAccountButton, c);
         add(panel);
 
-        //event listener 
         loginButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 login();
@@ -74,21 +73,70 @@ public class HotelReservationSystem extends JFrame {
     private void login() {
         String username = usernameField.getText();
         String password = String.valueOf(passwordField.getPassword());
-
-
+        
         JOptionPane.showMessageDialog(this, "You have successfully logged in.");
+    
+        
+        JFrame editAccountFrame = new JFrame("Edit Account Information");
+        editAccountFrame.setSize(400, 400);
+        editAccountFrame.setLocationRelativeTo(null);
+    
+        
+        JPanel editAccountPanel = new JPanel(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        c.gridx = 0;
+        c.gridy = 0;
+        c.gridwidth = 2;
+        c.insets = new Insets(10, 10, 30, 10);
+        editAccountPanel.add(new JLabel("Edit Account Information"), c);
+    
+        
+        c.gridx = 0;
+        c.gridy = 1;
+        c.gridwidth = 1;
+        c.insets = new Insets(10, 10, 10, 10);
+        editAccountPanel.add(new JLabel("Name:"), c);
+        c.gridx = 1;
+        editAccountPanel.add(new JTextField(20), c);
+    
+        
+        c.gridx = 0;
+        c.gridy = 2;
+        editAccountPanel.add(new JLabel("Phone Number:"), c);
+        c.gridx = 1;
+        editAccountPanel.add(new JTextField(20), c);
+    
+        
+        c.gridx = 0;
+        c.gridy = 3;
+        editAccountPanel.add(new JLabel("Email:"), c);
+        c.gridx = 1;
+        editAccountPanel.add(new JTextField(20), c);
+    
+        
+        c.gridx = 0;
+        c.gridy = 4;
+        editAccountPanel.add(new JLabel("Payment Method:"), c);
+        c.gridx = 1;
+        editAccountPanel.add(new JComboBox<>(new String[]{"Credit Card", "PayPal"}), c);
+    
+        
+        c.gridx = 0;
+        c.gridy = 5;
+        c.gridwidth = 2;
+        editAccountPanel.add(new JButton("Save Changes"), c);
+    
+        
+        editAccountFrame.add(editAccountPanel);
+    
+        
+        editAccountFrame.setVisible(true);
     }
-
-    private void createAccount() {
-        String username = usernameField.getText();
-        String password = String.valueOf(passwordField.getPassword());
-
-        JOptionPane.showMessageDialog(this, "You have created a new account.");
-    }
-
+    
     public static void main(String[] args) {
         HotelReservationSystem app = new HotelReservationSystem();
         app.setVisible(true);
     }
 }
+    
  */
