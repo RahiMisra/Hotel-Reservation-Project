e9fa1200e8a0890999e3cd6f0298899b099cabac
Rahi <125520697+RahiMisra@users.noreply.github.com>
2023-04-18 17:04:38 -0700

    Add files via upload
    a few functions for backend of user stories

---
+++ Phase 3 Grind/src/Reservation.java
@@ -0,0 +1,31 @@
+
+public class Reservation {
+	private int room;
+	private String date;
+	private int price;
+	private String username;
+	public int getRoom() {
+		return room;
+	}
+	public void setRoom(int room) {
+		this.room = room;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+}
