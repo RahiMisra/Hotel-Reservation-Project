c08a3330b61a30fab9be04b88546bf8be775b3a7
Mohammad Asim H. Sheikh <M.asimhsheikh@gmail.com>
2023-04-18 21:46:26 -0700

    Deleted useless files

--- mohammad documentation/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"<Unnamed>","c":"HotelReservationSystem","l":"HotelReservationSystem()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"HotelReservationSystem","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
