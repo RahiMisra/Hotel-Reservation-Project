6b731af23be485d58d60710562feb1c11e6f9bbe
Ortega4 <124750763+Ortega4@users.noreply.github.com>
2023-04-26 17:53:08 -0700

    Update AvailableRooms.java

--- alejandro documentation/src/AvailableRooms.java
+++ alejandro documentation/src/AvailableRooms.java
@@ -1,40 +1,28 @@
 /**
  *
- * The AvailableRooms class represents rooms in  a hotel with rooms of various types.
- *Rooms can be reserved or released, and their availability can be checked.
+ *
+ * The AvailableRooms class represents the available rooms in a hotel.
+ *
+ * It keeps track of the number of rooms available for each room type and the number of occupied rooms for each type.
+ *
+ *
  *@author (Alejandro Ortega)
  *@version (1.2)
- *
  */
+
 public class AvailableRooms {
-    /**
-     * represents the maximum amount of king bedrooms available.
-     */
+
     private final int maxKingBedsAvailable;
-    /**
-     * represents the maximum amount of queen bedrooms available.
-     */
     private final int maxQueenBedsAvailable;
-    /**
-     * represents the maximum amount of suite bedrooms available.
-     */
     private final int maxSuiteBedsAvailable;
-    /**
-     * represents  king bedrooms occupied.
-     */
     private int kingBedsOccupied;
-    /**
-     * represents  queen bedrooms occupied.
-     */
     private int queenBedsOccupied;
-    /**
-     * represents  suite bedrooms occupied.
-     */
     private int suiteBedsOccupied;
 
     /**
-     * Constructs a new AvailableRooms object with default values.
-     * There are 20 king-sized beds, 20 queen-sized beds, and 10 suite beds available.
+     * Constructs an AvailableRooms object with default values.
+     * There are 20 king rooms, 20 queen rooms, and 10 suite rooms available.
+     * All the rooms are initially unoccupied.
      */
     public AvailableRooms() {
         maxKingBedsAvailable = 20;
@@ -46,16 +34,17 @@ public class AvailableRooms {
     }
 
     /**
-     * Constructs a new AvailableRooms object with specified maximum bed and occupancy values.
-     * @param maxKingBedsAvailable the maximum number of king-sized beds available
-     * @param maxQueenBedsAvailable the maximum number of queen-sized beds available
-     * @param maxSuiteBedsAvailable the maximum number of suite beds available
-     * @param kingBedsOccupied the current number of occupied king-sized beds
-     * @param queenBedsOccupied the current number of occupied queen-sized beds
-     * @param suiteBedsOccupied the current number of occupied suite beds
+     * Constructs an AvailableRooms object with the specified number of available and occupied rooms for each type.
+     *
+     * @param maxKingBedsAvailable  the maximum number of king rooms available
+     * @param maxQueenBedsAvailable the maximum number of queen rooms available
+     * @param maxSuiteBedsAvailable the maximum number of suite rooms available
+     * @param kingBedsOccupied      the number of king rooms that are currently occupied
+     * @param queenBedsOccupied     the number of queen rooms that are currently occupied
+     * @param suiteBedsOccupied     the number of suite rooms that are currently occupied
      */
-
-    public AvailableRooms(int maxKingBedsAvailable, int maxQueenBedsAvailable, int maxSuiteBedsAvailable, int kingBedsOccupied, int queenBedsOccupied, int suiteBedsOccupied) {
+    public AvailableRooms(int maxKingBedsAvailable, int maxQueenBedsAvailable, int maxSuiteBedsAvailable,
+                          int kingBedsOccupied, int queenBedsOccupied, int suiteBedsOccupied) {
         this.maxKingBedsAvailable = maxKingBedsAvailable;
         this.maxQueenBedsAvailable = maxQueenBedsAvailable;
         this.maxSuiteBedsAvailable = maxSuiteBedsAvailable;
@@ -65,110 +54,97 @@ public class AvailableRooms {
     }
 
     /**
-     * Reserves a king-sized bed room.
+     * Reserves a room of the specified type.
+     *
+     * @param room the type of room to reserve (king, queen, or suite)
      */
-    public void reserveKingBedRoom() {
-        kingBedsOccupied++;
+    public void reserveRoom(String room) {
+        if (room.equals("king")) {
+            kingBedsOccupied++;
+        } else if (room.equals("queen")) {
+            queenBedsOccupied++;
+        } else if (room.equals("suite")) {
+            suiteBedsOccupied++;
+        }
     }
 
     /**
-     * Reserves a queen-sized bed room.
+     * Releases a room of the specified type.
+     *
+     * @param room the type of room to release (king, queen, or suite)
      */
-    public void reserveQueenBedRoom() {
-        queenBedsOccupied++;
+    public void releaseRoom(String room) {
+        if (room.equals("king")) {
+            kingBedsOccupied--;
+        } else if (room.equals("queen")) {
+            queenBedsOccupied--;
+        } else if (room.equals("suite")) {
+            suiteBedsOccupied--;
+        }
     }
 
     /**
-     * Reserves a suite bed room.
+     * Checks if there is at least one room of the specified type available.
+     *
+     * @param roomtype the type of room to check availability for (king, queen, or suite)
+     * @return true if at least one room of the specified type is available, false otherwise
      */
-    public void reserveSuiteBedRoom() {
-        suiteBedsOccupied++;
-    }
+    public boolean isRoomTypeAvailable(String roomtype) {
+        boolean isAvailable = false;
+        if (roomtype == "king") {
+            isAvailable = KingRoomsLeft() > 0;
+        } else if (roomtype == "queen") {
+            isAvailable = QueenRoomsLeft() > 0;
+        } else if (roomtype == "suite") {
+            isAvailable = QueenRoomsLeft() > 0;
+        }
+        return isAvailable;
 
-    /**
-     * Releases a king-sized bed room.
-     */
-    public void releaseKingBedRoom() {
-        kingBedsOccupied--;
-    }
-
-    /**
-     * Releases a queen-sized bed room.
-     */
-    public void releaseQueenBedRoom() {
-        queenBedsOccupied--;
-    }
-
-    /**
-     * Releases a suite bed room.
-     */
-    public void releaseSuiteBedRoom() {
-        suiteBedsOccupied--;
-    }
-
-    /**
-     * Returns true if at least one king-sized bed room is available, false otherwise.
-     * @return true if a king-sized bed room is available, false otherwise
-     */
-    public boolean isKingBedAvailable() {
-        return KingRoomsLeft() > 0;
-    }
-
-    /**
-     * Returns true if at least one queen-sized bed room is available, false otherwise.
-     * @return true if a queen-sized bed room is available, false otherwise
-     */
-    public boolean isQueenBedAvailable() {
-        return QueenRoomsLeft() > 0;
     }
 
 
-    /**
-     * checks to see if suite bedrooms are available, false otherwise.
-     * @return true if a suite bed room is available, false otherwise
-     */
-    public boolean isSuiteBedAvailable() {
-        if (SuiteRoomsLeft() >0) {
-
-            return true;
-        } else
-            return false;
-    }
     /**
      * gets the number of king bed rooms available.
+     *
      * @return the number of king size bed rooms available.
      */
-    public int KingRoomsLeft(){
-        return maxKingBedsAvailable-kingBedsOccupied;
-    }
-    /**
-     * gets the number of Queen bed rooms available.
-     * @return the number of Queen size bed rooms available.
-     */
-    public int QueenRoomsLeft(){
-        return maxQueenBedsAvailable  -queenBedsOccupied;
-    }
-    /**
-     * gets the number of Suite bed rooms available.
-     * @return the number of Queen size bed rooms available.
-     */
-    public int SuiteRoomsLeft(){
-        return maxSuiteBedsAvailable-suiteBedsOccupied;
+    public int KingRoomsLeft() {
+        return maxKingBedsAvailable - kingBedsOccupied;
     }
 
+    /**
+     * gets the number of Queen bed rooms available.
+     *
+     * @return the number of Queen size bed rooms available.
+     */
+    public int QueenRoomsLeft() {
+        return maxQueenBedsAvailable - queenBedsOccupied;
+    }
+
+    /**
+     * gets the number of Suite bed rooms available.
+     *
+     * @return the number of Queen size bed rooms available.
+     */
+    public int SuiteRoomsLeft() {
+        return maxSuiteBedsAvailable - suiteBedsOccupied;
+    }
 
     /**
      * provides a string to the number of king, Queen, and suite bed rooms available.
+     *
      * @return a list of available rooms.
      */
-    public  String roomsAvailable(){
+    public String roomsAvailable() {
 
 
-        return "kingrooms available "+KingRoomsLeft() +"\n"
+        return "kingrooms available " + KingRoomsLeft() + "\n"
                 + "queenrooms available: " + QueenRoomsLeft() + "\n"
-                + "suiterooms available: "+ SuiteRoomsLeft() +"\n";
+                + "suiterooms available: " + SuiteRoomsLeft() + "\n";
     }
 
-
-
 }
+
+
+
+
