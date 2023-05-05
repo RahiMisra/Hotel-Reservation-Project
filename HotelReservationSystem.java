7263be7971003597b58d8bca6cd670ac6cddaf68
Mohammad Asim H. Sheikh <M.asimhsheikh@gmail.com>
2023-05-04 22:58:01 -0700

    Added SQL DB to createAccount() and Login()

--- HotelReservationSystem.java
+++ HotelReservationSystem.java
@@ -1,11 +1,13 @@
-/*
- 
 import javax.swing.*;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.event.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class HotelReservationSystem extends JFrame {
     private JButton loginButton, createAccountButton, contactStaffButton;
@@ -82,55 +84,72 @@ public class HotelReservationSystem extends JFrame {
     private void login() {
         String username = usernameField.getText();
         String password = String.valueOf(passwordField.getPassword());
-
-        JOptionPane.showMessageDialog(this, "You have successfully logged in.");
-
-        JFrame editAccountFrame = new JFrame("Edit Account Information");
-        editAccountFrame.setSize(400, 400);
-        editAccountFrame.setLocationRelativeTo(null);
-
-        JPanel editAccountPanel = new JPanel(new GridBagLayout());
-        GridBagConstraints c = new GridBagConstraints();
-        c.gridx = 0;
-        c.gridy = 0;
-        c.gridwidth = 2;
-        c.insets = new Insets(10, 10, 30, 10);
-        editAccountPanel.add(new JLabel("Edit Account Information"), c);
-
-        c.gridx = 0;
-        c.gridy = 1;
-        c.gridwidth = 1;
-        c.insets = new Insets(10, 10, 10, 10);
-        editAccountPanel.add(new JLabel("Name:"), c);
-        c.gridx = 1;
-        editAccountPanel.add(new JTextField(20), c);
-        c.gridx = 0;
-        c.gridy = 2;
-        editAccountPanel.add(new JLabel("Phone Number:"), c);
-        c.gridx = 1;
-        editAccountPanel.add(new JTextField(20), c);
     
-        c.gridx = 0;
-        c.gridy = 3;
-        editAccountPanel.add(new JLabel("Email:"), c);
-        c.gridx = 1;
-        editAccountPanel.add(new JTextField(20), c);
+        Connection con = DatabaseMethods.getConnection(); // get a database connection
+        if (DatabaseMethods.testDatabaseconnection(con)) { // check if the connection is successful
+            String sql = "SELECT * FROM users WHERE username=? AND password=?";
+            try {
+                PreparedStatement stmt = con.prepareStatement(sql);
+                stmt.setString(1, username);
+                stmt.setString(2, password);
+                ResultSet rs = stmt.executeQuery();
+                if (rs.next()) { // user exists in database
+                    JOptionPane.showMessageDialog(this, "You have successfully logged in.");
     
-        c.gridx = 0;
-        c.gridy = 4;
-        editAccountPanel.add(new JLabel("Payment Method:"), c);
-        c.gridx = 1;
-        editAccountPanel.add(new JComboBox<>(new String[]{"Credit Card", "PayPal"}), c);
+                    JFrame editAccountFrame = new JFrame("Edit Account Information");
+                    editAccountFrame.setSize(400, 400);
+                    editAccountFrame.setLocationRelativeTo(null);
     
-        c.gridx = 0;
-        c.gridy = 5;
-        c.gridwidth = 2;
-        editAccountPanel.add(new JButton("Save Changes"), c);
+                    JPanel editAccountPanel = new JPanel(new GridBagLayout());
+                    GridBagConstraints c = new GridBagConstraints();
+                    c.gridx = 0;
+                    c.gridy = 0;
+                    c.gridwidth = 2;
+                    c.insets = new Insets(10, 10, 30, 10);
+                    editAccountPanel.add(new JLabel("Edit Account Information"), c);
     
-        editAccountFrame.add(editAccountPanel);
+                    c.gridx = 0;
+                    c.gridy = 1;
+                    c.gridwidth = 1;
+                    c.insets = new Insets(10, 10, 10, 10);
+                    editAccountPanel.add(new JLabel("Name:"), c);
+                    c.gridx = 1;
+                    editAccountPanel.add(new JTextField(20), c);
+                    c.gridx = 0;
+                    c.gridy = 2;
+                    editAccountPanel.add(new JLabel("Phone Number:"), c);
+                    c.gridx = 1;
+                    editAccountPanel.add(new JTextField(20), c);
     
-        editAccountFrame.setVisible(true);
+                    c.gridx = 0;
+                    c.gridy = 3;
+                    editAccountPanel.add(new JLabel("Email:"), c);
+                    c.gridx = 1;
+                    editAccountPanel.add(new JTextField(20), c);
+    
+                    c.gridx = 0;
+                    c.gridy = 4;
+                    editAccountPanel.add(new JLabel("Payment Method:"), c);
+                    c.gridx = 1;
+                    editAccountPanel.add(new JComboBox<>(new String[]{"Credit Card", "PayPal"}), c);
+    
+                    c.gridx = 0;
+                    c.gridy = 5;
+                    c.gridwidth = 2;
+                    editAccountPanel.add(new JButton("Save Changes"), c);
+    
+                    editAccountFrame.add(editAccountPanel);
+    
+                    editAccountFrame.setVisible(true);
+                } else { // user does not exist in database
+                    JOptionPane.showMessageDialog(this, "Invalid username or password. Please try again.");
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
     }
+    
 
     private void createAccount() {
         JTextField firstNameField = new JTextField(20);
@@ -192,7 +211,7 @@ public class HotelReservationSystem extends JFrame {
         panel.add(new JButton("Create Account"), c);
     
         int result = JOptionPane.showConfirmDialog(null, panel, "Create Account",
-            JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
+        JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
     
         if (result == JOptionPane.OK_OPTION) {
             String firstName = firstNameField.getText();
@@ -201,18 +220,28 @@ public class HotelReservationSystem extends JFrame {
             String email = emailField.getText();
             String password = String.valueOf(passwordField.getPassword());
             String paymentMethod = (String) paymentMethodCombo.getSelectedItem();
-            
-            // Add this to database 
-            System.out.println("Account created:");
-            System.out.println("First Name: " + firstName);
-            System.out.println("Last Name: " + lastName);
-            System.out.println("Phone Number: " + phoneNumber);
-            System.out.println("Email: " + email);
-            System.out.println("Password: " + password);
-            System.out.println("Payment Method: " + paymentMethod);
+
+        createAccountButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                    // code to save user information to database goes here
+                    // this code will be executed when the createAccountButton is clicked
+             
+            Connection con = DatabaseMethods.getConnection(); // get a database connection
+            if (DatabaseMethods.testDatabaseconnection(con)) { // check if the connection is successful
+                String firstName = "";
+                String lastName = "";
+                String phoneNumber = "";
+                String email = "";
+                String password = "";
+                String paymentMethod = "";
+                DatabaseMethods.createAccount(con, firstName,lastName, phoneNumber, email, password, paymentMethod);
+                JOptionPane.showMessageDialog(panel,"Account created successfully!");
+            }
+        }
+    });
+
         }
     }
-
     
     private void contactStaff() {
         JFrame contactStaffFrame = new JFrame("Contact Staff");
@@ -262,6 +291,3 @@ public class HotelReservationSystem extends JFrame {
         app.setVisible(true);
     }
 }    
-
-
- */
